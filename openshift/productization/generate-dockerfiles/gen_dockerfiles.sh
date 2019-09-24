6c7fd94d2c42bbf460017a8875c5c5431c46b12e
Warren Van Winckel <wvanwinc@redhat.com>
2019-09-23 19:57:44 -0700

    Update Makefile, and replace m4 with envsubst

--- openshift/productization/generate-dockerfiles/gen_dockerfiles.sh
+++ openshift/productization/generate-dockerfiles/gen_dockerfiles.sh
@@ -1,7 +1,14 @@
 #!/bin/bash -x
 
+target_dir=$1
+
 for component in serving; do
     for subcomponent in controller autoscaler activator networking-istio networking-certmanager webhook queue; do
-        m4 -DCOMPONENT=$component -DSUBCOMPONENT=$subcomponent Dockerfile.m4 > ../dist-git/Dockerfile.$subcomponent
+        CAPITALIZED_COMPONENT=$(echo -e "$component" | sed -r 's/\<./\U&/g') \
+        CAPITALIZED_SUBCOMPONENT=$(echo -e "$subcomponent" | sed -r 's/\<./\U&/g') \
+        GO_PACKAGE=$(echo -e "$subcomponent" | sed -r 's/-/\//g') \
+        COMPONENT=$component \
+        SUBCOMPONENT=$subcomponent \
+        envsubst < openshift/productization/generate-dockerfiles/Dockerfile.in > ${target_dir}/Dockerfile.$subcomponent
     done
 done
