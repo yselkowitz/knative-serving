5b76d799b7a290a6553861ad5e8540c295acc2dc
Warren Van Winckel <wvanwinc@redhat.com>
2019-09-25 11:03:02 -0700

    Add new YAML files to release branching scripts

--- openshift/release/create-release-branch.sh
+++ openshift/release/create-release-branch.sh
@@ -17,9 +17,9 @@ git checkout -b "$target" "$release"
 
 # Update openshift's master and take all needed files from there.
 git fetch openshift master
-git checkout openshift/master -- openshift OWNERS_ALIASES OWNERS Makefile
+git checkout openshift/master -- openshift OWNERS_ALIASES OWNERS Makefile content_sets.yml container.yaml
 make generate-dockerfiles
 make RELEASE=$release generate-release
 make RELEASE=ci generate-release
-git add openshift OWNERS_ALIASES OWNERS Makefile
+git add openshift OWNERS_ALIASES OWNERS Makefile content_sets.yml container.yaml
 git commit -m "Add openshift specific files."
