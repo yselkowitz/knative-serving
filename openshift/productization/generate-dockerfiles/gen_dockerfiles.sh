1b7835dcccd1a274018a35e0e1a39f12bcdc7653
Warren Van Winckel <wvanwinc@redhat.com>
2019-10-29 21:23:07 -0700

    Fix autoscaler-hpa Dockerfile in generator

--- openshift/productization/generate-dockerfiles/gen_dockerfiles.sh
+++ openshift/productization/generate-dockerfiles/gen_dockerfiles.sh
@@ -2,12 +2,18 @@
 
 target_dir=$1
 
-component=serving
-for subcomponent in controller autoscaler autoscaler-hpa activator networking-istio networking-certmanager networking-nscert webhook queue; do
-    CAPITALIZED_COMPONENT=$(echo -e "$component" | sed -r 's/\<./\U&/g') \
-    CAPITALIZED_SUBCOMPONENT=$(echo -e "$subcomponent" | sed -r 's/\<./\U&/g') \
-    GO_PACKAGE=$(echo -e "$subcomponent" | sed -r 's/-/\//g') \
-    COMPONENT=$component \
-    SUBCOMPONENT=$subcomponent \
-    envsubst < openshift/productization/generate-dockerfiles/Dockerfile.in > ${target_dir}/Dockerfile.$subcomponent
+component=Serving
+
+for subcomponent in Activator Autoscaler Autoscaler-HPA \
+                    Networking-Istio Networking-CertManager Networking-NSCert \
+                    Queue Webhook; \
+do
+    CAPITALIZED_COMPONENT=$component \
+    CAPITALIZED_SUBCOMPONENT=$(echo -e "$subcomponent" | sed -e 's/-/ /g') \
+    COMPONENT=$(echo -e "$component" | sed -e 's/\(.*\)/\L\1/g') \
+    SUBCOMPONENT=$(echo -e "$subcomponent" | sed -e 's/\(.*\)/\L\1/g') \
+    GO_PACKAGE=$(echo -e "$SUBCOMPONENT" | sed -e 's/networking-/networking\//g') \
+    envsubst \
+      < openshift/productization/generate-dockerfiles/Dockerfile.in \
+      > ${target_dir}/Dockerfile.$subcomponent
 done
