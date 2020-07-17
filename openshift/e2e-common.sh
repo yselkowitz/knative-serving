9b1d0b35045163c096dabdc74d87503c8b44fea9
Kenjiro Nakayama <nakayamakenjiro@gmail.com>
2020-07-17 18:27:26 +0900

    Add replica option to HA tests (#502)
    * [WIP] Add replica option to ha tests
    
    * Use buckets size 1 until chaosduck is deployed
    
    * Remove wait_for_leader_controller
    
    * Use multiple buckets
    
    * Back to buckets 1
    
    * Remove default value from replicas and buckets
    
    * Remove comment line

--- openshift/e2e-common.sh
+++ openshift/e2e-common.sh
@@ -191,6 +191,25 @@ function run_e2e_tests(){
   local test_name=$1 
   local failed=0
 
+  # Keep this in sync with test/ha/ha.go
+  readonly REPLICAS=2
+  # TODO: Increase BUCKETS size more than 1 when operator supports configmap/config-leader-election setting.
+  readonly BUCKETS=1
+
+  # Changing the bucket count and cycling the controllers will leave around stale
+  # lease resources at the old sharding factor, so clean these up.
+  kubectl -n ${SYSTEM_NAMESPACE} delete leases --all
+
+  # Wait for a new leader Controller to prevent race conditions during service reconciliation
+  wait_for_leader_controller || failed=1
+
+  # Dump the leases post-setup.
+  header "Leaders"
+  kubectl get lease -n "${SYSTEM_NAMESPACE}"
+
+  # Give the controller time to sync with the rest of the system components.
+  sleep 30
+
   if [ -n "$test_name" ]; then
     go_test_e2e -tags=e2e -timeout=15m -parallel=1 \
     ./test/e2e ./test/conformance/api/... ./test/conformance/runtime/... \
@@ -226,16 +245,16 @@ function run_e2e_tests(){
   # Prevent HPA from scaling to make the tests more stable
   oc -n "$SERVING_NAMESPACE" patch hpa activator \
   --type 'merge' \
-  --patch '{"spec": {"maxReplicas": '2', "minReplicas": '2'}}' || return 1
-
-  # Give the controller time to sync with the rest of the system components.
-  sleep 30
+  --patch '{"spec": {"maxReplicas": '${REPLICAS}', "minReplicas": '${REPLICAS}'}}' || return 1
 
   # Use sed as the -spoofinterval parameter is not available yet
   sed "s/\(.*requestInterval =\).*/\1 10 * time.Millisecond/" -i vendor/knative.dev/pkg/test/spoof/spoof.go
 
+  # Run HA tests separately as they're stopping core Knative Serving pods
+  # Define short -spoofinterval to ensure frequent probing while stopping pods
   go_test_e2e -tags=e2e -timeout=15m -failfast -parallel=1 \
     ./test/ha \
+    -replicas="${REPLICAS}" -buckets="${BUCKETS}" -spoofinterval="10ms" \
     --kubeconfig "$KUBECONFIG" \
     --imagetemplate "$TEST_IMAGE_TEMPLATE" \
     --resolvabledomain "$(ingress_class)"|| failed=3
