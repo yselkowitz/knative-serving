b99c9829380ec235c8ae1db2fd3a590cc624aa77
Markus Thömmes <markusthoemmes@me.com>
2021-05-11 14:24:03 +0200

    Add openshift specific files.

---
+++ openshift/release/resolve.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+function resolve_resources(){
+  local dir=$1
+  local resolved_file_name=$2
+
+  echo "Writing resolved yaml to $resolved_file_name"
+
+  > "$resolved_file_name"
+
+  for yaml in `find $dir -name "*.yaml" | sort`; do
+    resolve_file "$yaml" "$resolved_file_name"
+  done
+}
+
+function resolve_file() {
+  local file=$1
+  local to=$2
+
+  # Skip nscert, it's not part of upstream's release YAML either.
+  if grep -q 'networking.knative.dev/wildcard-certificate-provider: nscert' "$1"; then
+    return
+  fi
+
+  echo "---" >> "$to"
+  # 1. Rewrite image references
+  # 2. Update config map entry
+  # 3. Replace serving.knative.dev/release label.
+  sed -e "s+serving.knative.dev/release: devel+serving.knative.dev/release: \"v0.21.0\"+" \
+      "$file" >> "$to"
+}
