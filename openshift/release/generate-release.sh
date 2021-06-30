90b639c68cb1c13e66a068d134bd9acc78721bdd
Kenjiro Nakayama <nakayamakenjiro@gmail.com>
2021-06-30 15:43:44 +0900

    Add v0.23 label to manifests (#833)
    * Remove config/domainmapping
    
    * Add 0.23 label

--- openshift/release/generate-release.sh
+++ openshift/release/generate-release.sh
@@ -5,4 +5,4 @@ source $(dirname $0)/resolve.sh
 release=$1
 output_file="openshift/release/knative-serving-${release}.yaml"
 
-resolve_resources "config/core/ config/hpa-autoscaling/ config/domain-mapping/" "$output_file"
+resolve_resources "config/core/ config/hpa-autoscaling/" "$output_file"
