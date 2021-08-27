16a5c14ea850cc0b75c40d748803fafb0fe08278
Kenjiro Nakayama <nakayamakenjiro@gmail.com>
2021-08-27 15:11:42 +0900

    Fix invalid script (#898)

--- openshift/e2e-common.sh
+++ openshift/e2e-common.sh
@@ -305,7 +305,7 @@ function run_e2e_tests(){
 
   # Give the controller time to sync with the rest of the system components.
   sleep 30
-  subdomain=$(oc get ingresses.config.openshift.io cluster  -o jsonpath="{.spec.domain}"
+  subdomain=$(oc get ingresses.config.openshift.io cluster  -o jsonpath="{.spec.domain}")
 
   if [ -n "$test_name" ]; then
     go_test_e2e -tags=e2e -timeout=15m -parallel=1 \
