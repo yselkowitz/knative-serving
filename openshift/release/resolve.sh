a971687caf548854a5274c7c0b5e4195f4584959
Stavros Kontopoulos <skontopo@redhat.com>
2021-12-16 14:29:07 +0200

    Add openshift specific files.

---
+++ openshift/release/resolve.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+function resolve_resources(){
+  local dir=$1
+  local resolved_file_name=$2
+
+  echo "Writing resolved yaml to $resolved_file_name"
+
+  > "$resolved_file_name"
+
+  for yaml in `find $dir -name "*.yaml" | sort`; do
+    resolve_file "$yaml" "$resolved_file_name"
+  done
+}
+
+function resolve_file() {
+  local file=$1
+  local to=$2
+
+  echo "---" >> "$to"
+  # 1. Rewrite image references
+  # 2. Update config map entry
+  # 3. Replace serving.knative.dev/release label.
+  sed -e "s+serving.knative.dev/release: devel+serving.knative.dev/release: \"v0.26.0\"+" \
+      -e "s+app.kubernetes.io/version: devel+app.kubernetes.io/version: \"v0.26.0\"+" \
+      "$file" >> "$to"
+}
