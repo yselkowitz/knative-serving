6c2520b46edf0c4555e4a11b43b5a61b676fc8be
Stavros Kontopoulos <st.kontopoulos@gmail.com>
2021-12-20 17:52:31 +0200

    [RELEASE-v1.0.1] Update resolve version for 1.0.1 (#1008)
    * update resolve version etc
    
    * try v1.0.1

--- openshift/release/resolve.sh
+++ openshift/release/resolve.sh
@@ -21,7 +21,7 @@ function resolve_file() {
   # 1. Rewrite image references
   # 2. Update config map entry
   # 3. Replace serving.knative.dev/release label.
-  sed -e "s+serving.knative.dev/release: devel+serving.knative.dev/release: \"v0.26.0\"+" \
-      -e "s+app.kubernetes.io/version: devel+app.kubernetes.io/version: \"v0.26.0\"+" \
+  sed -e "s+serving.knative.dev/release: devel+serving.knative.dev/release: \"v1.0.1\"+" \
+      -e "s+app.kubernetes.io/version: devel+app.kubernetes.io/version: \"v1.0.1\"+" \
       "$file" >> "$to"
 }
