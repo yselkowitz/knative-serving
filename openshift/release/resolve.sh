7e612b35a9b3294d2716eb0a2407448b96e34b94
Kenjiro Nakayama <nakayamakenjiro@gmail.com>
2022-06-03 23:13:01 +0900

    Bump version label in manifest (#1136)

--- openshift/release/resolve.sh
+++ openshift/release/resolve.sh
@@ -21,7 +21,7 @@ function resolve_file() {
   # 1. Rewrite image references
   # 2. Update config map entry
   # 3. Replace serving.knative.dev/release label.
-  sed -e "s+serving.knative.dev/release: devel+serving.knative.dev/release: \"v1.2.0\"+" \
-      -e "s+app.kubernetes.io/version: devel+app.kubernetes.io/version: \"v1.2.0\"+" \
+  sed -e "s+serving.knative.dev/release: devel+serving.knative.dev/release: \"v1.3.0\"+" \
+      -e "s+app.kubernetes.io/version: devel+app.kubernetes.io/version: \"v1.3.0\"+" \
       "$file" >> "$to"
 }
