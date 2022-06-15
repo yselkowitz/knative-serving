a7bbf6376daa4e08cf8871ee9ca40df6748ef38d
OpenShift Cherrypick Robot <openshift-cherrypick-robot@redhat.com>
2022-06-15 15:28:43 -0700

    [release-v1.4] Require 32 consecutive OK responses before having the endpoint ready (#1145)
    * Require 32 consecutive OK responses before having the endpoint ready
    
    * The point is that we do not add the RouteInconsistencyRetryChecker to
    the general Poll function but only to Check function which is used in
    CheckEndpointState. So tests that directly call client.Do should not be
    sending many requests.
    
    * Separate simple and robust retry checker
    
    * Use the multi/robust checker in CheckEndpointState
    * Use the simple checker in simple client.Do/Poll calls
    
    Co-authored-by: Martin Gencur <mgencur@redhat.com>

--- vendor/knative.dev/pkg/test/spoof/openshift_checks.go
+++ vendor/knative.dev/pkg/test/spoof/openshift_checks.go
@@ -20,3 +20,21 @@ func RouteInconsistencyRetryChecker(resp *Response) (bool, error) {
 	}
 	return false, nil
 }
+
+// RouteInconsistencyMultiRetryChecker retries common requests seen when creating a new route
+// - 503 to account for Openshift route inconsistency (https://jira.coreos.com/browse/SRVKS-157)
+func RouteInconsistencyMultiRetryChecker() ResponseChecker {
+	const neededSuccesses = 32
+	var successes int
+	return func(resp *Response) (bool, error) {
+		if resp.StatusCode == http.StatusServiceUnavailable {
+			successes = 0
+			return true, fmt.Errorf("retrying route inconsistency request: %s", resp)
+		}
+		successes++
+		if successes < neededSuccesses {
+			return true, fmt.Errorf("successful requests: %d, required: %d", successes, neededSuccesses)
+		}
+		return false, nil
+	}
+}
